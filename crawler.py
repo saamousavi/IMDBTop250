f3f3d1ec93e77249357402f8fedb94763e307714
amirali mousavi <mousavi.amirali1377@gmail.com>
2023-06-03 10:43:07 +0330

    complete __init__ function and something changed

--- crawler.py
+++ crawler.py
@@ -9,14 +9,19 @@ import os
 
 class IMDBCrawler:
     def __init__(self):
-        self.baseURL = 'https://www.imdb.com/'
+        self.baseURL = 'https://www.imdb.com'
         self.data_path = './Data'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             , 'accept-language': 'en-US'
         }
-        self.top250_movies_url = pd.DataFrame(columns=['id', 'title', 'url'])
-        self.dataframe_names = ['top250_movies']
+        self.dataframe_names = ['movies_url', 'movie_details', 'movie_details', 'genre', 'cast', 'crew']
+        self.movies_url = pd.DataFrame(columns=['id', 'url'])
+        self.movie_details = pd.DataFrame(columns=['id', 'title', 'year', 'runtime', 'parental_guide', 'budget'])
+        self.person_details = pd.DataFrame(columns=['id', 'name'])
+        self.genre = pd.DataFrame(columns=['movie_id', 'genre'])
+        self.cast = pd.DataFrame(columns=['movie_id', 'person_id'])
+        self.crew = pd.DataFrame(columns=['movie_id', 'person_id', 'role'])
 
     def start_crawling(self):
         self.create_data_directory()
@@ -46,7 +51,9 @@ class IMDBCrawler:
         df.to_csv(file_path, index=False)
 
     def get_top250_movies_urls(self):
-        url = self.baseURL + 'chart/top/?ref_=nv_mv_250'
+        print("get_top250_movies_urls started")
+
+        url = self.baseURL + '/chart/top/?ref_=nv_mv_250'
         req_failed_count = 0
         while req_failed_count < 3:
             try:
@@ -57,14 +64,22 @@ class IMDBCrawler:
                 soup = BeautifulSoup(response.content, 'html.parser')
                 for movie in soup.select('.titleColumn'):
                     url = movie.find('a').get('href')
-                    title = movie.find('a').text
                     id = url.split('/')[2]
-                    self.top250_movies_url.loc[len(self.top250_movies_url)] = [id, title, url]
+                    self.movies_url.loc[len(self.movies_url)] = [id, url]
 
                 self.save_csv('top250_movies')
 
             except Exception as e:
                 req_failed_count += 1
+                print(e)
+
+        print("get_top250_movies_urls finished")
+
+    def scrap_movie_page(self, url):
+        pass
+
+    def crawl_movies_url(self):
+        pass
 
 
 if __name__ == '__main__':
